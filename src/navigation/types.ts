0ad140f229241a6774f8b3a6518b936771ab22dd
jpjitendrapal <jpjitendrapal@gmail.com>
2026-05-05 15:47:24 +0530

    working project with app and website

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -7,6 +7,7 @@ export type RootStackParamList = {
   SignUp: undefined;
   OTPVerification: { mobile: string; name?: string; isSignUp?: boolean };
   ProductDetail: { productId: number };
+  Cart: undefined;
 };
 
 export type RootNavigationProp = NativeStackNavigationProp<RootStackParamList>;
